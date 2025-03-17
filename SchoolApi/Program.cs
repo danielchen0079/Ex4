97f23486a84b90ad231f5717feacd66a8380123a
danielchen0079 <53239213+danielchen0079@users.noreply.github.com>
2025-03-17 23:30:35 +1300

    Update Program.cs

--- SchoolApi/Program.cs
+++ SchoolApi/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 app.MapControllers();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
